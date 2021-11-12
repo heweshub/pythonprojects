a0c29f261e8c3dea438beaca33d13e04826f9794
heweshub <wendahe1996@163.com>
2021-11-12 10:40:18 +0800

    完成飞船的创建

---
+++ alien_invasion/settings.py
@@ -0,0 +1,13 @@
+# -*- coding: UTF-8 -*-
+"""
+author: cjhcw
+"""
+
+
+class Settings:
+
+    def __init__(self):
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+
